07ba456d2ca3c4ac102121c03da76353de9d32cb
JonasHill01 <jonashiller2001@gmail.com>
2023-12-10 16:03:04 +0100

    init

---
+++ src/test/java/de/jonashill01/FitnessApp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jonashill01.FitnessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
